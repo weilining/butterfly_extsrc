0670aa665ac0363b30a188e2eda02d318ec8a8b6
Jerry <hwy0127@gmail.com>
2020-12-05 18:59:08 +0800

    update animejs to v3.2.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,18 @@
-var gulp = require('gulp')
-// var uglify = require('gulp-uglify')
-// var babel = require('gulp-babel')
-var rename = require('gulp-rename')
-var uglifyjs = require('terser')
-var composer = require('gulp-uglify/composer')
-var pump = require('pump')
-var minify = composer(uglifyjs, console)
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const terser = require('gulp-terser');
 
-// minify js babel
-// gulp.task('compress', () =>
-//   gulp.src('./src/**/*.js')
-//     .pipe(babel({
-//       presets: ['@babel/preset-env']
-//     }))
-//     .pipe(rename({
-//       suffix: '.min'
-//     }))
-//     .pipe(uglify().on('error', function (e) {
-//       console.log(e)
-//     }))
-//     .pipe(gulp.dest('./dist'))
-// )
-
-gulp.task('compress', function (cb) {
-  var options = {}
-  pump([
-    gulp.src('./src/**/*.js')
+// minify js 
+gulp.task('compress', () =>
+  gulp.src('./src/**/*.js')
+    .pipe(terser())
     .pipe(rename({
       suffix: '.min'
-    })),
-    minify(options),
-    gulp.dest('./dist')
-  ],
-  cb
-  )
-})
+    }))
+    .pipe(gulp.dest('./dist'))
+)
 
-// 執行 gulp 命令時執行的任務
+
 // 執行 gulp 命令時執行的任務
 gulp.task('default', gulp.parallel(
   'compress'
